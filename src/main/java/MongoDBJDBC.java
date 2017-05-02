f3a2667c2b80a9136e1ecc5d10ff5ff7b6b7d59a
chenqinqin <chenqinqin@shbj.com>
2017-05-02 16:40:49 +0800

    mongodb java

--- src/main/java/MongoDBJDBC.java
+++ src/main/java/MongoDBJDBC.java
@@ -1,6 +1,9 @@
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 import java.util.ArrayList;
@@ -18,13 +21,20 @@ public class MongoDBJDBC {
 //        System.out.println("Create collection successfully");
         MongoCollection<Document> collection = mongoDatabase.getCollection("col");
         System.out.println("choose col collection successfully");
-        Document document = new Document("title", "MongoDB")
-                .append("description","database")
-                .append("likes", 100)
-                .append("by","Fly");
-        List<Document> documents = new ArrayList<Document>();
-        documents.add(document);
-        collection.insertMany(documents);
-        System.out.println("insert document successfully");
+//        Document document = new Document("title", "MongoDB")
+//                .append("description","database")
+//                .append("likes", 100)
+//                .append("by","Fly");
+//        List<Document> documents = new ArrayList<Document>();
+//        documents.add(document);
+//        collection.insertMany(documents);
+//        System.out.println("insert document successfully");
+//        collection.updateMany(Filters.eq("likes", 100), new Document("$set", new Document("likes", 200)));
+        collection.deleteOne(Filters.eq("likes", 200));
+        FindIterable<Document> findIterable = collection.find();
+        MongoCursor<Document> mongoCursor = findIterable.iterator();
+        while (mongoCursor.hasNext()) {
+            System.out.println(mongoCursor.next());
+        }
     }
 }
